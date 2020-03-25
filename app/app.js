08706580c3f6c9a4412f460c2191995941d45f4b
drakos <4613678+Jolly-Pirate@users.noreply.github.com>
2020-03-25 19:24:48 -0400

    VWAP calculation

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const
   Reset = "\x1b[0m",
-  Red = "\x1b[31m",
+  Blue = "\x1b[34m",
   Green = "\x1b[32m",
+  Red = "\x1b[31m",
   Yellow = "\x1b[33m",
+  Underscore = "\x1b[4m",
   request = require('request'),
   steem = require('steem'),
   config = require('./config.json'),
@@ -50,39 +52,6 @@ checkAccount = function () {
   });
 };
 
-getPrice = function (exchange, ticker_url) {
-  return new Promise((resolve, reject) => {
-    request(ticker_url, function (error, response, body) {
-      //console.log(response);
-      if (body) {
-        var json = JSON.parse(body);
-        if (exchange === "binance")
-          resolve(parseFloat(json.price));
-        if (exchange === "bittrex")
-          resolve(parseFloat(json.result.Last));
-        if (exchange === "huobi")
-          resolve(parseFloat(json.tick.close));
-        if (exchange === "krakenUSDT")
-          resolve(parseFloat(json.result.USDTZUSD.c[0]));
-        if (exchange === "poloniexSTEEM")
-          resolve(json.BTC_STEEM.last);
-        if (exchange === "poloniexBTC")
-          resolve(parseFloat(json.USDT_BTC.last));
-        if (exchange === "upbit")
-          resolve(json[0].tradePrice);
-        if (exchange === "ionomy")
-          resolve(parseFloat(json.data.price));
-        if (exchange === "probit")
-          resolve(parseFloat(json.data[0].last));
-      }
-      if (error) {
-        console.log(Red, error, Reset);
-        resolve(0); // set the price to 0 then take care of not calculating it in the price average
-      }
-    });
-  });
-};
-
 (async function () {
   var status = await checkAccount();
   if (status)
@@ -104,132 +73,187 @@ async function priceFeed() {
   if (config.hivechain) {
     token = 'HIVE';
     if (config.bittrex) {
-      var bittrexUsdtBtc = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=usdt-btc");
-      var bittrexBtcHive = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=btc-hive");
-      var bittrexPrice = bittrexUsdtBtc * bittrexBtcHive;
+      var bittrexUsdtBtc = await utils.getPrice("bittrex", "usdt-btc");
+      var bittrexBtcToken = await utils.getPrice("bittrex", "btc-hive");
+      var bittrexPrice = bittrexUsdtBtc.price * bittrexBtcToken.price;
+      var bittrexVolume = bittrexBtcToken.volume;
     }
     if (config.ionomy) {
       // Ionomy doesn't have a USDT/BTC pair, so using the average from exchanges
-      var binanceBtcUsdt = await getPrice("binance", "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-      var bittrexUsdtBtc = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=usdt-btc");
-      var poloniexUsdtBtc = await getPrice("poloniexBTC", "https://poloniex.com/public?command=returnTicker");
-      var upbitUsdtBtc = await getPrice("upbit", "https://crix-api.upbit.com/v1/crix/trades/ticks?code=CRIX.UPBIT.USDT-BTC");
-
-      var averageUsdtBtc = (binanceBtcUsdt + bittrexUsdtBtc + poloniexUsdtBtc + upbitUsdtBtc) / 4;
-
-      var ionomyBtcHive = await getPrice("ionomy", "https://ionomy.com/api/v1/public/market-summary?market=btc-hive");
-      var ionomyPrice = averageUsdtBtc * ionomyBtcHive;
+      var binanceBtcUsdt = await utils.getPrice("binance", "BTCUSDT");
+      var bittrexUsdtBtc = await utils.getPrice("bittrex", "usdt-btc");
+      var poloniexUsdtBtc = await utils.getPrice("poloniex", "USDT_BTC");
+      var upbitUsdtBtc = await utils.getPrice("upbit", "USDT-BTC");
+      var averageUsdtBtc = (binanceBtcUsdt.price + bittrexUsdtBtc.price + poloniexUsdtBtc.price + upbitUsdtBtc.price) / 4;
+      var ionomyBtcToken = await utils.getPrice("ionomy", "btc-hive");
+      var ionomyPrice = averageUsdtBtc * ionomyBtcToken.price;
+      var ionomyVolume = ionomyBtcToken.volume;
     }
-    if (config.probit) { // https://docs-en.probit.com/docs/getting-started
-      var probitUsdtHive = await getPrice("probit", "https://api.probit.com/api/exchange/v1/ticker?market_ids=HIVE-USDT");
-      var probitPrice = probitUsdtHive;
+    if (config.probit) {
+      var probitUsdtToken = await utils.getPrice("probit", "HIVE-USDT");
+      var probitPrice = probitUsdtToken.price;
     }
+
+    if (config.probit) {
+      var probitUsdtToken = await utils.getPrice("probit", "HIVE-USDT");
+      var probitPrice = probitUsdtToken.price;
+      var probitVolume = probitUsdtToken.volume;
+    }
+
   }
 
   if (config.steemchain) {
     token = 'STEEM';
     if (config.binance) {
-      var binanceSteemBtc = await getPrice("binance", "https://api.binance.com/api/v3/ticker/price?symbol=STEEMBTC");
-      var binanceBtcUsdt = await getPrice("binance", "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-      var binancePrice = binanceSteemBtc * binanceBtcUsdt;
+      var binanceBtcUsdt = await utils.getPrice("binance", "BTCUSDT");
+      var binanceTokenBtc = await utils.getPrice("binance", "STEEMBTC");
+      var binancePrice = binanceTokenBtc.price * binanceBtcUsdt.price;
+      var binanceVolume = binanceTokenBtc.volume;
     }
     if (config.bittrex) {
-      var bittrexUsdtBtc = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=usdt-btc");
-      var bittrexBtcSteem = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=btc-steem");
-      var bittrexPrice = bittrexUsdtBtc * bittrexBtcSteem;
+      var bittrexUsdtBtc = await utils.getPrice("bittrex", "usdt-btc");
+      var bittrexBtcToken = await utils.getPrice("bittrex", "btc-steem");
+      var bittrexPrice = bittrexUsdtBtc.price * bittrexBtcToken.price;
+      var bittrexVolume = bittrexBtcToken.volume;
     }
     if (config.huobi) {
-      var huobiSteemUsdt = await getPrice("huobi", "https://api.huobi.pro/market/detail/merged?symbol=steemusdt");
-      var huobiPrice = huobiSteemUsdt;
+      var huobiTokenUsdt = await utils.getPrice("huobi", "steemusdt");
+      var huobiPrice = huobiTokenUsdt.price;
+      var huobiVolume = huobiTokenUsdt.volume;
     }
     if (config.poloniex) {
-      var poloniexUsdtBtc = await getPrice("poloniexBTC", "https://poloniex.com/public?command=returnTicker");
-      var poloniexBtcSteem = await getPrice("poloniexSTEEM", "https://poloniex.com/public?command=returnTicker");
-      var poloniexPrice = poloniexUsdtBtc * poloniexBtcSteem;
+      var poloniexUsdtBtc = await utils.getPrice("poloniex", "USDT_BTC");
+      var poloniexBtcToken = await utils.getPrice("poloniex", "BTC_STEEM");
+      var poloniexPrice = poloniexUsdtBtc.price * poloniexBtcToken.price;
+      var poloniexVolume = poloniexBtcToken.volume;
     }
     if (config.upbit) {
-      var upbitUsdtBtc = await getPrice("upbit", "https://crix-api.upbit.com/v1/crix/trades/ticks?code=CRIX.UPBIT.USDT-BTC");
-      var upbitBtcSteem = await getPrice("upbit", "https://crix-api.upbit.com/v1/crix/trades/ticks?code=CRIX.UPBIT.BTC-STEEM");
-      var upbitPrice = upbitUsdtBtc * upbitBtcSteem;
+      var upbitUsdtBtc = await utils.getPrice("upbit", "USDT-BTC");
+      var upbitBtcToken = await utils.getPrice("upbit", "BTC-STEEM");
+      var upbitPrice = upbitUsdtBtc.price * upbitBtcToken.price;
+      var upbitVolume = upbitBtcToken.volume;
     }
     if (config.probit) {
-      var probitUsdtSteem = await getPrice("probit", "https://api.probit.com/api/exchange/v1/ticker?market_ids=STEEM-USDT");
-      var probitPrice = probitUsdtSteem;
+      var probitUsdtToken = await utils.getPrice("probit", "STEEM-USDT");
+      var probitPrice = probitUsdtToken.price;
+      var probitVolume = probitUsdtToken.volume;
     }
+    if (config.ionomy) {
+      // Ionomy doesn't have a USDT/BTC pair, so using the average from exchanges
+      var binanceBtcUsdt = await utils.getPrice("binance", "BTCUSDT");
+      var bittrexUsdtBtc = await utils.getPrice("bittrex", "usdt-btc");
+      var poloniexUsdtBtc = await utils.getPrice("poloniex", "USDT_BTC");
+      var upbitUsdtBtc = await utils.getPrice("upbit", "USDT-BTC");
+      var averageUsdtBtc = (binanceBtcUsdt.price + bittrexUsdtBtc.price + poloniexUsdtBtc.price + upbitUsdtBtc.price) / 4;
+      var ionomyBtcToken = await utils.getPrice("ionomy", "btc-steem");
+      var ionomyPrice = averageUsdtBtc * ionomyBtcToken.price;
+      var ionomyVolume = ionomyBtcToken.volume;
+    }
+
   }
 
   var priceArray = [];
+  var volumeArray = [];
 
-  console.log(Yellow + token + "/USDT prices" + Reset);
+  console.log(Blue + Underscore + token + "/USDT prices and volumes" + Reset);
 
-  if (bittrexPrice > 0) {
-    console.log(("Bittrex").padEnd(8), bittrexPrice.toFixed(3));
-    priceArray.push(bittrexPrice);
-  }
   if (binancePrice > 0) {
-    console.log(("Binance").padEnd(8), binancePrice.toFixed(3));
+    console.log(("Binance").padEnd(8), "$" + binancePrice.toFixed(3), binanceVolume.toFixed(3).padStart(12));
     priceArray.push(binancePrice);
+    volumeArray.push(binanceVolume);
+  }
+  if (bittrexPrice > 0) {
+    console.log(("Bittrex").padEnd(8), "$" + bittrexPrice.toFixed(3), bittrexVolume.toFixed(3).padStart(12));
+    priceArray.push(bittrexPrice);
+    volumeArray.push(bittrexVolume);
   }
   if (huobiPrice > 0) {
-    console.log(("Huobi").padEnd(8), huobiPrice.toFixed(3));
+    console.log(("Huobi").padEnd(8), "$" + huobiPrice.toFixed(3), huobiVolume.toFixed(3).padStart(12));
     priceArray.push(huobiPrice);
-  }
-  if (upbitPrice > 0) {
-    console.log(("UpBit").padEnd(8), upbitPrice.toFixed(3));
-    priceArray.push(upbitPrice);
-  }
-  if (poloniexPrice > 0) {
-    console.log(("Poloniex").padEnd(8), poloniexPrice.toFixed(3));
-    priceArray.push(poloniexPrice);
+    volumeArray.push(huobiVolume);
   }
   if (ionomyPrice > 0) {
-    console.log(("Ionomy").padEnd(8), ionomyPrice.toFixed(3));
+    console.log(("Ionomy").padEnd(8), "$" + ionomyPrice.toFixed(3), ionomyVolume.toFixed(3).padStart(12));
     priceArray.push(ionomyPrice);
+    volumeArray.push(ionomyVolume);
+  }
+  if (poloniexPrice > 0) {
+    console.log(("Poloniex").padEnd(8), "$" + poloniexPrice.toFixed(3), poloniexVolume.toFixed(3).padStart(12));
+    priceArray.push(poloniexPrice);
+    volumeArray.push(poloniexVolume);
   }
   if (probitPrice > 0) {
-    console.log(("Probit").padEnd(8), probitPrice.toFixed(3));
+    console.log(("Probit").padEnd(8), "$" + probitPrice.toFixed(3), probitVolume.toFixed(3).padStart(12));
     priceArray.push(probitPrice);
+    volumeArray.push(probitVolume);
+  }
+  if (upbitPrice > 0) {
+    console.log(("UpBit").padEnd(8), "$" + upbitPrice.toFixed(3), upbitVolume.toFixed(3).padStart(12));
+    priceArray.push(upbitPrice);
+    volumeArray.push(upbitVolume);
   }
 
-  // USDT correction. Kraken ticker info https://www.kraken.com/help/api#get-ticker-info
-  var bittrexUsdUsdt = await getPrice("bittrex", "https://bittrex.com/api/v1.1/public/getticker?market=usd-usdt");
-  var krakenUsdtUsd = await getPrice("krakenUSDT", "https://api.kraken.com/0/public/Ticker?pair=USDTUSD");
-  var averageUSDT = ((bittrexUsdUsdt + krakenUsdtUsd) / 2).toFixed(3);
+  // Volume Weighted Average Price (VWAP)
+  // VWAP= ∑(Price * Volume) / ∑Volume
 
-  // calculate the average
+  // USDT correction.
+  var bittrexUsdUsdt = await utils.getPrice("bittrex", "usd-usdt");
+  var krakenUsdtUsd = await utils.getPrice("kraken", "USDTUSD");
+
+  var averageUsdt = ((bittrexUsdUsdt.price + krakenUsdtUsd.price) / 2).toFixed(3);
+  var VWAPTUsdt = ((bittrexUsdUsdt.price * bittrexUsdUsdt.volume + krakenUsdtUsd.price * krakenUsdtUsd.volume) / (bittrexUsdUsdt.volume + krakenUsdtUsd.volume)).toFixed(3);
+
   var total = 0;
+  var totalPricexVolume = 0;
+  var totalVolume = 0;
+
   for (var i in priceArray) {
     total += priceArray[i];
+    totalPricexVolume += priceArray[i] * volumeArray[i];
+    totalVolume += volumeArray[i];
   }
-  var averagePriceUSDT = (total / priceArray.length).toFixed(3);
-  var averagePrice = (averagePriceUSDT * averageUSDT).toFixed(3);
 
-  console.log(Yellow + "AVERAGE USDT/USD  ", averageUSDT + Reset);
-  console.log(Yellow + "AVERAGE " + token + "/USDT", averagePriceUSDT, "(from", priceArray.length, "exchanges)" + Reset);
-  console.log(Green + "AVERAGE " + token + "/USD ", averagePrice + Reset);
+  // VWAP
+  var VWAP = (totalPricexVolume / totalVolume).toFixed(3);
+  var adjustedVWAP = (VWAP * VWAPTUsdt).toFixed(3);
 
-  var base = averagePrice + " SBD";
+  // average
+  var averagePriceUsdt = (total / priceArray.length).toFixed(3);
+  var adjustedAverage = (averagePriceUsdt * averageUsdt).toFixed(3);
+
+
+  console.log(Blue + Underscore + "Average Price", "(from", priceArray.length, "exchanges)" + Reset);
+  console.log(Yellow + "USDT/USD  ", averageUsdt + Reset);
+  console.log(Yellow + token + "/USDT", averagePriceUsdt + Reset);
+  console.log(Green + token + "/USD ", adjustedAverage + Reset);
+
+  console.log(Blue + Underscore + "Volume Weighted Average Price (VWAP)", "(from", priceArray.length, "exchanges)" + Reset);
+  console.log(Yellow + "USDT/USD  ", VWAPTUsdt + Reset);
+  console.log(Yellow + token + "/USDT", VWAP + Reset);
+  console.log(Green + token + "/USD ", adjustedVWAP + Reset);
+
+  var base = adjustedVWAP + " SBD";
 
   var quote = "1.000";
   if (config.peg) {
     var percentage = ((1 - config['peg_multi']) * 100).toFixed(2);
     console.log('Pegging is enabled. Adjusting price by ' + percentage + '% (set config.peg to false to disable)');
-    console.log('Price before the peg bias : ', averagePrice);
+    console.log('Price before the peg bias : ', adjustedVWAP);
     quote = (1 / config['peg_multi']).toFixed(3);
-    console.log('Price after the peg bias  : ', (averagePrice / quote).toFixed(3));
+    console.log('Price after the peg bias  : ', (adjustedVWAP / quote).toFixed(3));
   }
 
   exchangeRate = {base: base, quote: quote + " STEEM"};
 
 
   if (config.testmode) {
-    console.log(Green + utils.getDate(), 'Price feed ' + token + '/USD $' + averagePrice + Reset);
+    console.log(Green + utils.getDate(), 'Price feed ' + token + '/USD $' + adjustedVWAP + Reset);
     console.log(Red + 'TEST MODE ON, NOTHING IS BROADCAST' + Reset);
   } else {
     steem.broadcast.feedPublishAsync(config.privateActiveKey, config.witness, exchangeRate)
       .then(function (data) {
         if (data)
-          console.log(Green + utils.getDate(), 'Published price feed ' + token + '/USD $' + averagePrice + Reset);
+          console.log(Green + utils.getDate(), 'Published price feed ' + token + '/USD $' + adjustedVWAP + Reset);
       })
       .catch(function (err) {
         console.log(utils.getDate(), Red, "Error in feedPublishAsync()", err, Reset);
--- app/utils.js
+++ app/utils.js
@@ -1,5 +1,6 @@
 const
   steem = require('steem'),
+  request = require('request'),
   config = require('./config.json');
 
 var getAccountInfo = function (account) {
@@ -56,10 +57,94 @@ function switchrpc(counter) {
   }
 }
 
+
+function getPrice(exchange, pair) {
+  return new Promise((resolve, reject) => {
+    var url;
+    switch (exchange) {
+      case "bittrex":
+        // https://bittrex.github.io/api/v1-1
+        url = "https://api.bittrex.com/api/v1.1/public/getmarketsummary?market=" + pair;
+        break;
+      case "binance":
+        // https://binance-docs.github.io/apidocs/spot/en/
+        url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + pair;
+        break;
+      case "huobi":
+        // https://huobiapi.github.io/docs/dm/v1/en/
+        url = "https://api.huobi.pro/market/detail/merged?symbol=" + pair;
+        break;
+      case "poloniex":
+        // https://docs.poloniex.com
+        url = "https://poloniex.com/public?command=returnTicker";
+        break;
+      case "upbit":
+        // https://docs.upbit.com/v1.0.7/reference
+        // https://upbit.com/exchange?code=CRIX.UPBIT.BTC-STEEM
+        url = "https://api.upbit.com/v1/ticker?markets=" + pair;
+        break;
+
+      case "probit":
+        // https://docs-en.probit.com/docs/getting-started
+        // https://www.probit.com/app/exchange/STEEM-USDT
+        url = "https://api.probit.com/api/exchange/v1/ticker?market_ids=" + pair;
+        break;
+
+      case "ionomy":
+        // docurl
+        url = "https://ionomy.com/api/v1/public/market-summary?market=" + pair;
+        break;
+
+      case "kraken":
+        // https://www.kraken.com/help/api#get-ticker-info
+        url = "https://api.kraken.com/0/public/Ticker?pair=" + pair;
+        break;
+
+      default:
+      // code block
+    }
+    request(url, function (error, response, body) {
+      //console.log(response);
+      if (body) {
+        var json = JSON.parse(body);
+
+        if (exchange === "binance")
+          resolve({price: parseFloat(json.lastPrice), volume: parseFloat(json.volume)});
+
+        if (exchange === "bittrex")
+          resolve({price: parseFloat(json.result[0].Last), volume: parseFloat(json.result[0].Volume)});
+
+        if (exchange === "huobi")
+          resolve({price: parseFloat(json.tick.close), volume: parseFloat(json.tick.vol)});
+
+        if (exchange === "kraken")
+          resolve({price: parseFloat(json.result.USDTZUSD.c[0]), volume: parseFloat(json.result.USDTZUSD.v[0])});
+
+        if (exchange === "poloniex")
+          resolve({price: parseFloat(json[pair].last), volume: parseFloat(json[pair].quoteVolume)}); // using variable in json selector
+
+        if (exchange === "upbit")
+          resolve({price: json[0].trade_price, volume: json[0].acc_trade_volume_24h});
+
+        if (exchange === "ionomy") {
+          resolve({price: parseFloat(json.data.price), volume: parseFloat(json.data.volume)});
+        }
+        if (exchange === "probit")
+          resolve({price: parseFloat(json.data[0].last), volume: parseFloat(json.data[0].base_volume)});
+      }
+      if (error) {
+        console.log(error);
+        resolve(0); // set the price to 0 then take care of not calculating it in the price average
+      }
+    });
+  });
+}
+
 module.exports = {
   getAccountInfo,
   checkPrivateKey,
   getDate,
   wait,
-  switchrpc
+  switchrpc,
+  getPrice
 };

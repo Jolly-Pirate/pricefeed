4d7fb40cad7776b24e52b35772d21654518114b3
Jolly-Pirate <4613678+Jolly-Pirate@users.noreply.github.com>
2023-03-02 00:21:36 -0500

    node lts and run.sh tweaks
    - Switched to using node lts-slim to reduce the final size of the image.
    - Formatting and tweaking of run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:19
+FROM node:lts-slim
 
 RUN useradd -ms /bin/bash pricefeed -G sudo
 RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 COPY package*.json ./
-RUN npm install && npm install -g pm2
+RUN npm install && npm install -g pm2 npm@9.5.1
 
 USER pricefeed
 WORKDIR /home/pricefeed
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 
 # function for warning($1 = text)
 function boldtext() {
-  echo -e `tput bold`"$1"`tput sgr0` >&2
+  echo -e $(tput bold)"$1"$(tput sgr0) >&2
 }
 
 DOCKER_NAME="pricefeed"
@@ -15,23 +15,25 @@ NORMAL=$(tput sgr0)
 #echo "Bash version ${BASH_VERSION}..."
 
 if [[ ! -f app/config.json ]]; then
-  echo -e "\e[1m\e[34m app/config.json missing, copying example \e[0m";
+  echo -e "\e[1m\e[34m app/config.json missing, copying example \e[0m"
   cp app/config.json.example app/config.json
   chmod 600 app/config.json
-  echo -e "\e[1m\e[34m Edit the app/config.json file before starting the app \e[0m";
+  echo -e "\e[1m\e[34m Edit the app/config.json file before starting the app \e[0m"
   exit 1
 fi
 
 build() {
   boldtext "Building docker container"
-  time docker build -t pricefeed .
+  docker build --no-cache -t pricefeed .
 }
 
 rebuild() {
   boldtext "Rebuilding docker container"
   docker rmi pricefeed
-  time docker build -t pricefeed .
-  docker ps -a | grep 'Exited' | awk '{print $1}' | xargs docker rm ; docker images | grep '<none>' | awk '{print $3}' | xargs docker rmi -f
+  docker build --no-cache -t pricefeed .
+  # clean up containers and images remnants
+  docker ps -a | grep 'Exited' | awk '{print $1}' | xargs docker rm
+  docker images | grep '<none>' | awk '{print $3}' | xargs docker rmi -f
 }
 
 install_docker() {
@@ -48,16 +50,23 @@ install_docker() {
 #The container name has to be after all of the arguments.
 
 start() {
-  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && boldtext "Container already started"  ||  ( boldtext "Starting container..." && docker run -itd --restart always -h $DOCKER_NAME -e TZ=$(cat /etc/timezone) -v $(pwd)/app:/home/pricefeed/app -u `stat -c "%u:%g" app` --name $DOCKER_NAME pricefeed)
+  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && boldtext "Container already started" || (
+    boldtext "Starting container..."
+    docker run -itd --restart always -h $DOCKER_NAME -e TZ=$(cat /etc/timezone) -v $(pwd)/app:/home/pricefeed/app -u $(stat -c "%u:%g" app) --name $DOCKER_NAME pricefeed
+  )
   logs
 }
 
 stop() {
-  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && ( boldtext "Stopping container..."  ; docker stop $DOCKER_NAME ; docker rm -f $DOCKER_NAME ) || boldtext "Container not running"
+  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && (
+    boldtext "Stopping container..."
+    docker kill $DOCKER_NAME
+    docker rm -f $DOCKER_NAME
+  ) || boldtext "Container not running"
 }
 
 enter() {
-  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && ( docker start $DOCKER_NAME && docker exec -it $DOCKER_NAME bash ) || boldtext "Container not running"
+  [[ ! -z $(docker ps -a | grep $DOCKER_NAME) ]] && (docker start $DOCKER_NAME && docker exec -it $DOCKER_NAME bash) || boldtext "Container not running"
 }
 
 logs() {
@@ -82,33 +91,33 @@ help() {
 }
 
 case $1 in
-  install_docker)
-    install_docker
+install_docker)
+  install_docker
   ;;
-  build)
-    build
+build)
+  build
   ;;
-  rebuild)
-    rebuild
+rebuild)
+  rebuild
   ;;
-  start)
-    start
+start)
+  start
   ;;
-  restart)
-    stop
-    start
+restart)
+  stop
+  start
   ;;
-  stop)
-    stop
+stop)
+  stop
   ;;
-  logs)
-    logs
+logs)
+  logs
   ;;
-  enter)
-    enter
+enter)
+  enter
   ;;
-  *)
-    echo "Invalid cmd"
-    help
+*)
+  echo "Invalid cmd"
+  help
   ;;
 esac
